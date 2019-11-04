abbbbe4780900a82eddca3f69caecf69ba964cad
Gaurav <gauravjoshi1998@gmail.com>
2019-11-04 17:41:31 +0530

    Add existing file

---
+++ assets/js/skills.js
@@ -0,0 +1,2 @@
+/*skills5 */
+$('.counter-count').each(function(){$(this).prop('Counter',0).animate({Counter:$(this).text()},{duration:2000,easing:'swing',step:function(now){$(this).text(Math.ceil(now))}})})
